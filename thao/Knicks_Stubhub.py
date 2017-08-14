686a8ce323691ca650c433aa81380d3ec9c58cb5
dududai <haotianyu1104@gmail.com>
2017-08-14 16:13:29 -0400

    first upload of Bob

---
+++ thao/Knicks_Stubhub.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import sqlalchemy
+import time
+import requests
+import base64
+import json
+import pprint
+import pandas as pd
+import numpy as np
+import datetime
+import os
+
+
+engine = sqlalchemy.create_engine("mysql+mysqldb://Rangers:19931104@rangers.cbtjzutpd1ig.us-east-2.rds.amazonaws.com:3306/Rangers")
+
+app_token = "a7e64ebe-0998-3dcd-afa8-88b5594440c9"
+consumer_key = "ebxARAhZKo2_4abI3vroPlkUOTEa"
+consumer_secret = "TuefeH8yl8rhzjXnBg1spQD4qmsa"
+stubhub_username = "haotianyu1104@gmail.com"
+stubhub_password = "Dududai1104"
+combo = consumer_key + ':' + consumer_secret
+basic_authorization_token = base64.b64encode(combo.encode('utf-8'))
+headers = {
+        'Content-Type':'application/x-www-form-urlencoded',
+        'Authorization':'Basic '+basic_authorization_token.decode('utf-8'),}
+body = {
+        'grant_type':'password',
+        'username':stubhub_username,
+        'password':stubhub_password,
+        'scope':'PRODUCTION'}
+url = 'https://api.stubhub.com/login'
+r = requests.post(url, headers=headers, data=body)
+token_respoonse = r.json()
+access_token = token_respoonse['access_token']
+user_GUID = r.headers['X-StubHub-User-GUID']
+inventory_url = 'https://api.stubhub.com/search/inventory/v2'
+
+headers['Authorization'] = 'Bearer ' + access_token
+headers['Accept'] = 'application/json'
+headers['Accept-Encoding'] = 'application/json'
+
+id_df=pd.DataFrame()
+search_url = 'https://api.stubhub.com/search/catalog/events/v3?name=knicks&parking=false&city=New York&rows=100&status=active'
+search = requests.get(search_url, headers=headers)
+search_dict= search.json()
+id_df=id_df.append(pd.DataFrame(search_dict['events']))
+
+
+for id in id_df['id']:    
+    eventid = '%s'%id
+    listing_df=pd.DataFrame()
+
+    for i in np.arange(0,2,1):
+        data = {'eventid':eventid,'start':250*i,'rows':250}
+        inventory = requests.get(inventory_url, headers=headers, params=data)
+        inv = inventory.json()
+        time.sleep(7)
+        if 'listing' in inv :
+            listing_df = listing_df.append(pd.DataFrame(inv['listing']))
+        else:
+            continue
+    t=datetime.datetime.now()
+    listing_df['listingPrice'] = listing_df.apply(lambda x: x['listingPrice']['amount'], axis=1)
+    listing_df['currentPrice'] = listing_df.apply(lambda x: x['currentPrice']['amount'], axis=1)
+    listing_df['eventName'] = id_df.loc[id_df['id']==int(eventid),'name'].iloc[0]
+    listing_df['eventDate'] = id_df.loc[id_df['id']==int(eventid),'eventDateLocal'].iloc[0][:10] 
+    listing_df['timestamp'] = t
+    if 'listingAttributeCategoryList' in listing_df and 'faceValue' in listing_df:
+        my_col = ['isGA','deliveryMethodList', 'deliveryTypeList', 'dirtyTicketInd','faceValue','listingAttributeCategoryList','score',
+                       'listingAttributeList','sectionId','sellerOwnInd','sellerSectionName','splitOption','splitVector','ticketSplit','zoneId','zoneName']
+    elif 'listingAttributeCategoryList' in listing_df:
+        my_col = ['isGA','deliveryMethodList', 'deliveryTypeList', 'dirtyTicketInd','listingAttributeCategoryList', 'score',
+                       'listingAttributeList','sectionId','sellerOwnInd','sellerSectionName','splitOption','splitVector','ticketSplit','zoneId','zoneName']
+    elif 'faceValue' in listing_df:
+        my_col = ['isGA','deliveryMethodList', 'deliveryTypeList', 'dirtyTicketInd','faceValue', 'score',
+                       'listingAttributeList','sectionId','sellerOwnInd','sellerSectionName','splitOption','splitVector','ticketSplit','zoneId','zoneName']
+    else:
+        my_col = ['isGA','deliveryMethodList', 'deliveryTypeList', 'dirtyTicketInd', 'score',
+                       'listingAttributeList','sectionId','sellerOwnInd','sellerSectionName','splitOption','splitVector','ticketSplit','zoneId','zoneName']        
+    result=listing_df.drop(my_col,axis=1)
+    result.to_sql(name='Knicks', con=engine, if_exists = 'append', index=False)
---
+++ thao/database_category.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 15 13:55:32 2017
+
+@author: haot
+"""
+
+import pandas as pd
+import numpy as np
+
+distinct=pd.read_csv("c:/project/fnb label/how many food sold.csv")
+
+
+
+beer=pd.read_csv("c:/project/fnb label/beer.csv")
+wine=pd.read_csv("c:/project/fnb label/wine.csv",encoding = "ISO-8859-1")
+soda=pd.read_csv("c:/project/fnb label/soda.csv")
+water=pd.read_csv("c:/project/fnb label/water.csv")
+mer=pd.read_csv("c:/project/fnb label/mer.csv")
+
+for i in np.arange(1,100,1):
+    distinct['volume'][distinct['product'].str.contains('%d oz' % i,case=False)]='%d oz' % i
+    distinct['volume'][distinct['product'].str.contains('%doz' % i,case=False)]='%d oz' % i
+
+ 
+for i in [ '%.1f' % elem for elem in np.arange(1,100,0.1) ]:
+        distinct['volume'][distinct['product'].str.contains('%s oz' % i,case=False)]='%s oz' % i
+        distinct['volume'][distinct['product'].str.contains('%soz' % i,case=False)]='%s oz' % i
+
+for i in [ '%.2f' % elem for elem in np.arange(1,100,0.01) ]:
+        distinct['volume'][distinct['product'].str.contains('%s oz' % i,case=False)]='%s oz' % i
+        distinct['volume'][distinct['product'].str.contains('%soz' % i,case=False)]='%s oz' % i
+
+for i in np.arange(1,100,1):
+    distinct['volume'][distinct['product'].str.contains('%d pcs' % i,case=False)]='%d oz' % i
+    distinct['volume'][distinct['product'].str.contains('%dpc' % i,case=False)]='%d oz' % i
+    
+
+
+distinct['volume'][distinct['product'].str.contains('187ML',case=False)]='187 ml'        
+        
+resultb=distinct['product'].str.contains('|'.join(beer['name']),case=False)
+distinct['category']=np.where(resultb==True,'beer','other')
+
+resulta=distinct['product'].str.contains('|'.join(wine['name']),case=False)
+distinct['category'][resulta]='wine'
+
+results=distinct['product'].str.contains('|'.join(soda['name']),case=False)
+distinct['category'][results]='soda'
+
+resultw=distinct['product'].str.contains('|'.join(water['name']),case=False)
+distinct['category'][resultw]='water'
+
+resultm=distinct['product'].str.contains('|'.join(mer['name']),case=False)
+distinct['category'][resultm]='merchandise'
+
+distinct['category'][distinct['product'].str.contains('FREDS RUBY RED|tito|liq|Scotch|SANGRIA|VODKA|MARGARITA|bourbon|rum|gin|tequila|Whisky|GREY GOOSE|tonic',case=False)]='liquor'
+distinct['category'][distinct['product'].str.contains('iced tea',case=False)]='iced tea'
+distinct['category'][distinct['product'].str.contains('lemonade|LEMON',case=False)]='lemonade'
+distinct['category'][distinct['product'].str.contains('coffee',case=False)]='coffee'
+distinct['category'][distinct['product'].str.contains('juice',case=False)]='juice'
+distinct['category'][distinct['product'].str.contains('popcorn|CRACKERJACK',case=False)]='popcorn'
+distinct['category'][distinct['product'].str.contains('PRETZEL',case=False)]='PRETZEL'
+distinct['category'][distinct['product'].str.contains('nacho',case=False)]='nachos'
+distinct['category'][distinct['product'].str.contains('m&m|HI-CHEW|SKITTLES|TWIZZLER|REESE|KLONDIKE',case=False)]='candy'
+distinct['category'][distinct['product'].str.contains('CHIPS',case=False)]='chips'
+distinct['category'][distinct['product'].str.contains('CONE|MAGNUM|bar|CHERRY GARCIA|cup|icecream|ice cream',case=False)]='icecream'
+distinct['category'][distinct['product'].str.contains('POWERADE',case=False)]='energy drink'
+distinct['category'][distinct['product'].str.contains('DOG',case=False)]='hot dog'
+distinct['category'][distinct['product'].str.contains('cookie|oreo',case=False)]='cookie'
+distinct['category'][distinct['product'].str.contains('frys|fries',case=False)]='french fires'
+distinct['category'][distinct['product'].str.contains('FNGRS|fng|FINGERS',case=False)]='chicken fingers'
+distinct['category'][distinct['product'].str.contains('burger',case=False)]='hamburger'
+distinct['category'][distinct['product'].str.contains('piz',case=False)]='pizza'
+distinct['category'][distinct['product'].str.contains('noodles',case=False)]='noodles'
+distinct['category'][distinct['product'].str.contains('nuts',case=False)]='nuts'
+distinct['category'][distinct['product'].str.contains('deli|pastrami|SAND',case=False)]='sandwiches'
+distinct['category'][distinct['product'].str.contains('sushi',case=False)]='sushi'
+distinct['category'][distinct['product'].str.contains('jersey|jer ',case=False)]='jersey'
+distinct['category'][distinct['product'].str.contains('hat',case=False)]='hat'
+distinct['category'][distinct['product'].str.contains('pom|cuffed',case=False)]='cuffed pom'
+distinct['category'][distinct['product'].str.contains('tee',case=False)]='tee'
+distinct['category'][distinct['product'].str.contains('bear',case=False)]='bear'
+distinct['category'][distinct['product'].str.contains('puck',case=False)]='puck'
+distinct['category'][distinct['product'].str.contains('lanyard',case=False)]='lanyard'
+distinct['category'][distinct['product'].str.contains('foam finger',case=False)]='foam finger'
+distinct['category'][distinct['product'].str.contains('scarf',case=False)]='scarf'
+distinct['category'][distinct['product'].str.contains('replica|OYO',case=False)]='model'
+distinct['category'][distinct['product'].str.contains('glass',case=False)]='glass'
+distinct['category'][distinct['product'].str.contains('mug',case=False)]='mug'
+distinct['category'][distinct['product'].str.contains('decal sheet',case=False)]='decal sheet'
+distinct['category'][distinct['product'].str.contains('striker',case=False)]='striker'
+distinct['category'][distinct['product'].str.contains('hoopset',case=False)]='hoopset'
+distinct['category'][distinct['product'].str.contains('pullover',case=False)]='pullover'
+distinct['category'][distinct['product'].str.contains('hood',case=False)]='hoodie'
+distinct['category'][distinct['product'].str.contains('headband',case=False)]='headband'
+distinct['category'][distinct['product'].str.contains('wristband',case=False)]='wristband'
+distinct['category'][distinct['product'].str.contains('book',case=False)]='yearbook'
+
+distinct['volume'][distinct['product'].str.contains(' s ',case=False)]='s'
+distinct['volume'][distinct['product'].str.contains(' m ',case=False)]='m'
+distinct['volume'][distinct['product'].str.contains(' l ',case=False)]='l'
+distinct['volume'][distinct['product'].str.contains(' xl ',case=False)]='xl'
+distinct['volume'][distinct['product'].str.contains(' l/xl ',case=False)]='l/xl'
+
+
+
+distinct.to_csv('category and volume.csv', index=False)
